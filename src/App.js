3e2b18de4a03fb2ae97353a870c444a4e3c68c0a
BALAJIOSM <balajiosm@gmail.com>
2021-08-25 17:16:34 +0530

    first commit for react

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import { Userform } from './components/userform'
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Userform></Userform>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/userform.js
@@ -0,0 +1,12 @@
+
+
+export function Userform() {//This is the Controller/Component here
+    //Model objects - we can define here
+
+    return (//JSX - Extensible Java Script - HTML+JS - This is the VIEW part here
+        <div>
+            User Form <br />
+            <input value="Balaji"></input>
+        </div>
+    );
+}
